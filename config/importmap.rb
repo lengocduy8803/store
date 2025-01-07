3c410d95fa5a3b47def410b6c134aa6fdd972255
lnd <lengocduy8803@gmail.com>
2025-01-07 21:40:05 +0700

    bootstrap

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,4 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application"
-pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
-
