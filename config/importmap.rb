181b8ac126b4450df70bed92fb0b79c26948d90b
lnd <lengocduy8803@gmail.com>
2025-01-08 12:50:37 +0700

    csdl

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,14 @@
-pin "application"
+# pin "/builds/application", to: "/app/assets/builds/application.css"
+# pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+# pin "trix"
+# pin "@rails/actiontext", to: "https://cdn.jsdelivr.net/npm/@rails/actiontext@7.0.8/actiontext.esm.js"
+# pin "trix"
+# pin "@rails/actiontext", to: "actiontext.esm.js"
+
+
+# Pin các thư viện cần thiết
+pin "bootstrap-css", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-pin "trix"
- #pin "@rails/actiontext", to: "actiontext.esm.js"
- pin "@rails/actiontext", to: "https://cdn.jsdelivr.net/npm/@rails/actiontext@7.0.8/actiontext.esm.js"
-pin "trix"
-pin "@rails/actiontext", to: "actiontext.esm.js"
+pin "trix", to: "https://cdn.jsdelivr.net/npm/trix@2.0.0/dist/trix.js"
+pin "@rails/actiontext", to: "https://cdn.jsdelivr.net/npm/@rails/actiontext@7.0.8/actiontext.esm.js"
+pin "/builds/application", to: "/app/assets/builds/application.css"
